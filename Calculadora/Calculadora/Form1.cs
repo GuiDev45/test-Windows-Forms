86d9484f154fa248369a1c16f9c2d2013338a2f2
GuiDev45 <bortolettodev@gmail.com>
2022-07-07 11:04:24 -0300

    Teste Calculadora

---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        decimal valor1 = 0, valor2 = 0;
+        string operacao = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "0"; //Concatena o 0 dentro do campo.
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "4";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "5";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += "9";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text += ".";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor2 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+
+                //Poderia ser feito com switch case também.
+                if (operacao == "SOMA")
+                {
+                    txtResultado.Text = Convert.ToString(valor1 + valor2);
+                }
+                else if (operacao == "SUB")
+                {
+                    txtResultado.Text = Convert.ToString(valor1 - valor2);
+                }
+                else if (operacao == "MULTI")
+                {
+                    txtResultado.Text = Convert.ToString(valor1 * valor2);
+                }
+                else if (operacao == "DIV")
+                {
+                    txtResultado.Text = Convert.ToString(valor1 / valor2);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture); //Convertendo a string da caixa de texto para decimal.
+                txtResultado.Text = ""; //Limpar o primeiro valor na caixa.
+                operacao = "SOMA";
+                lblOperacao.Text = "+"; //Vai mostrar o sinal na label.
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SUB";
+                lblOperacao.Text = "-";
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor");
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "MULTI";
+                lblOperacao.Text = "x";
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor");
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "DIV";
+                lblOperacao.Text = "/";
+            }
+            else
+            {
+                MessageBox.Show("Digite um valor");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            lblOperacao.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = ""; //Limpa o campo de texto.
+            valor1 = 0; //Limpa o valor1.
+            valor2 = 0; //Limpa o valor2.
+            lblOperacao.Text = ""; //Limpa a label.
+        }
+
+    }
+}
